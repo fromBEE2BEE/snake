02efc0dcd5755bbfcb45d3709cbb3de919ed8cee
fromBEE2BEE <elenakorneychuk.mail@gmail.com>
2017-10-25 20:04:32 +0300

    Functions

--- Program.cs
+++ Program.cs
@@ -8,10 +8,34 @@ namespace Snake
 {
     class Program
     {
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
+            
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
             Console.ReadLine();
         }
+
+
+       
+
+
     }
 }
+
